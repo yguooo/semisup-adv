7871d5112507f8ed5b686e44807886300df07565
tensorsketch <tensorsketch@gmail.com>
2021-01-22 11:12:39 -0600

    add

---
+++ test.sh
@@ -0,0 +1,2 @@
+python robust_self_training.py --distance l_inf --epsilon 0.031 --model_dir rst_adv 
+python robust_self_training.py --aux_data_filename mnistm --distance l_inf --epsilon 0.031 --model_dir rst_adv 
